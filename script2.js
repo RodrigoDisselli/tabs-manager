3dcfb029d009c811b0d9e95cebc869099d5e3798
Rodrigo Almeida <35853479+RodrigoDisselli@users.noreply.github.com>
2020-01-14 18:06:38 -0300

    adição de filtro quebrado

--- script2.js
+++ script2.js
@@ -1,5 +1,6 @@
 var database = [{"pageTitle": "Google"},{"pageTitle": "Youtube"},{"pageTitle": "Batata doce"},{"pageTitle": "Medium - Artigo"},];
 const pagesList = document.getElementById("pages-list-id");
+const searchInput = document.getElementById("search-input");
 
 
 database.map(object => createListItem(object.pageTitle));
@@ -17,6 +18,22 @@ function createListItem(pageTitle) {
     pagesList.appendChild(li);
 }
 
+searchInput.addEventListener("keypress", (event) => {
+    filterListItem(event.target.value);
+    console.log("digitado: " + event.target.value)
+});
+
+function filterListItem(text){
+    let listItens = document.getElementsByTagName("li");
+    for(var i = 0; i < listItens.length; i ++){
+        pagesList.removeChild(listItens[i]);
+    }
+    
+    let result = database.filter( data => data.pageTitle.includes(text));
+    console.log("resultado:")
+    console.log(result);
+    result.map(object => createListItem(object.pageTitle));
+}
 
 
 // document.getElementById("pages-list-id").innerHTML = "<li> <div class='img' style='background-image: url(icon.png);'></div> <div><h3>Page Title</h3><h4>www.google.com.br/vini-otariohasuhsahuhdhsaudhduadsadh</h4></div></li>"
